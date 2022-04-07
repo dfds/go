016390f9aa575a66842ad476dd76f271cf0a9b62
Emil H. Clausen <sequoiia@hummel.yt>
2022-04-07 15:04:18 +0200

    Added helper funcs for Int32

--- utils/config/config.go
+++ utils/config/config.go
@@ -43,6 +43,20 @@ func GetEnvInt64(key string, def int64) (int64, error) {
 	return payload, nil
 }
 
+func GetEnvInt32(key string, def int32) (int32, error) {
+	val := os.Getenv(key)
+	if len(val) == 0 {
+		return def, nil
+	}
+
+	payload, err := strconv.ParseInt(val, 10, 32)
+	if err != nil {
+		return 0, err
+	}
+
+	return int32(payload), nil
+}
+
 func GetEnvBool(key string, def bool) bool {
 	val := os.Getenv(key)
 
