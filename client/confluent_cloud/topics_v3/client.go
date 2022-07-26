587906f07cf1cccc26e0cb15029cca515e835f4a
samidbb <samdi@dfds.com>
2022-07-26 07:54:21 +0200

    Topics CRUD

--- client/confluent_cloud/topics_v3/client.go
+++ client/confluent_cloud/topics_v3/client.go
@@ -25,12 +25,52 @@ func NewClient(defaultSession confluent_util.Session, defaultHttp *http.Client) 
 	}
 }
 
-// Create Request
-type CreateTopicsRequest struct {
+// func (e *ErrorResponseEntity) Error() string {
+// 	return e.Message
+// }
+
+// type ErrorResponseEntity struct {
+// 	ErrorCode int    `json:"error_code"`
+// 	Message   string `json:"message"`
+// }
+
+type TopicRequestEntity[T any] struct {
 	Endpoint  string
 	ClusterID string
-	Payload   CreateTopicsRequestPayload
+	TopicName *string `json:"topic_name,omitempty"`
+	Payload   *T
 }
+
+func (c *TopicRequestEntity[T]) Handle(e error, r *http.Response) (*confluent_util.ErrorResponseEntity, error) { // Will be called automatically on CreateTopicsRequest
+	if r.StatusCode != 204 && r.StatusCode >= 400 {
+		errorMessage, err := confluent_util.DeserializeResponse[confluent_util.ErrorResponseEntity](r.Body)
+		if err != nil {
+
+			return nil, err // TODO: need to return the original error
+		}
+		return &errorMessage, nil
+	}
+	return nil, nil
+}
+
+// Create Request
+// type CreateTopicsRequest struct {
+// 	Endpoint  string
+// 	ClusterID string
+// 	Payload   CreateTopicsRequestPayload
+// }
+
+// func (c *CreateTopicsRequest) Handle(e error, r *http.Response) *confluent_util.ErrorResponseEntity { // Will be called automatically on CreateTopicsRequest
+// 	fmt.Println("Printing statuscode")
+// 	fmt.Println(r.StatusCode)
+// 	if r.StatusCode != 204 && r.StatusCode >= 400 {
+// 		errorMessage, err := confluent_util.DeserializeResponse[confluent_util.ErrorResponseEntity](r.Body)
+// 		if err != nil {
+// 			return &errorMessage
+// 		}
+// 	}
+// 	return nil
+// }
 
 type CreateTopicsRequestPayload struct {
 	TopicName         string `json:"topic_name"`
@@ -65,7 +105,7 @@ type CreateTopicResponseEntity struct {
 	AuthorizedOperations []interface{} `json:"authorized_operations"`
 }
 
-func (c *TopicsClient) CreateKafkaTopic(session confluent_util.Session, request CreateTopicsRequest) (CreateTopicResponseEntity, error) {
+func (c *TopicsClient) CreateKafkaTopic(session confluent_util.Session, request TopicRequestEntity[CreateTopicsRequestPayload]) (CreateTopicResponseEntity, error) {
 	var payload CreateTopicResponseEntity
 	requestPayload, err := json.Marshal(request.Payload)
 	if err != nil {
@@ -75,14 +115,193 @@ func (c *TopicsClient) CreateKafkaTopic(session confluent_util.Session, request 
 	if err != nil {
 		return payload, err
 	}
-	payload, err = confluent_util.DoHttpRequest[CreateTopicResponseEntity](confluent_util.DoHttpRequestParameters{
+	r, errorResponseEntity, err := confluent_util.DoHttpRequest(confluent_util.DoHttpRequestParameters{
 		HttpClient:       c.http,
 		Req:              req,
 		ParameterSession: session,
 		DefaultSession:   c.defaultSession,
-	})
+	},
+		&request,
+	)
+
 	if err != nil {
 		return payload, err
 	}
+
+	if errorResponseEntity != nil {
+		return payload, errorResponseEntity
+	}
+
+	payload, err = confluent_util.DeserializeResponse[CreateTopicResponseEntity](r.Body)
+	if err != nil {
+		return payload, err
+	}
+
 	return payload, nil
+}
+
+// DELETE request
+type DeleteTopicsRequest struct {
+	Endpoint  string
+	ClusterID string
+	TopicName string
+}
+
+func (c *TopicsClient) DeleteKafkaTopic(session confluent_util.Session, request TopicRequestEntity[any]) error {
+	req, err := http.NewRequest("DELETE", fmt.Sprintf("%s/%s/%s/%s/%s", request.Endpoint, "kafka/v3/clusters", request.ClusterID, "topics", *request.TopicName), nil)
+	if err != nil {
+		return err
+	}
+	_, errorResponseEntity, err := confluent_util.DoHttpRequest(confluent_util.DoHttpRequestParameters{
+		HttpClient:       c.http,
+		Req:              req,
+		ParameterSession: session,
+		DefaultSession:   c.defaultSession,
+	}, &request)
+	if err != nil {
+		return err
+	}
+	if errorResponseEntity != nil {
+		return errorResponseEntity
+	}
+	return nil
+}
+
+// Get request
+type GetTopicRequest struct {
+	Endpoint  string
+	ClusterID string
+	TopicName string
+}
+
+type GetTopicResponseEntity struct { // Note: same as CreateTopicResponseEntity
+	Kind     string `json:"kind"`
+	Metadata struct {
+		Self         string `json:"self"`
+		ResourceName string `json:"resource_name"`
+	} `json:"metadata"`
+	ClusterID         string `json:"cluster_id"`
+	TopicName         string `json:"topic_name"`
+	IsInternal        bool   `json:"is_internal"`
+	ReplicationFactor int    `json:"replication_factor"`
+	PartitionsCount   int    `json:"partitions_count"`
+	Partitions        struct {
+		Related string `json:"related"`
+	} `json:"partitions"`
+	Configs struct {
+		Related string `json:"related"`
+	} `json:"configs"`
+	PartitionReassignments struct {
+		Related string `json:"related"`
+	} `json:"partition_reassignments"`
+}
+
+func (c *TopicsClient) GetTopicRequest(session confluent_util.Session, request TopicRequestEntity[any]) (GetTopicResponseEntity, error) {
+	var payload GetTopicResponseEntity
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/%s/%s/%s/%s", request.Endpoint, "kafka/v3/clusters", request.ClusterID, "topics", *request.TopicName), nil)
+	if err != nil {
+		return payload, err
+	}
+	r, errorResponseEntity, err := confluent_util.DoHttpRequest(confluent_util.DoHttpRequestParameters{
+		HttpClient:       c.http,
+		Req:              req,
+		ParameterSession: session,
+		DefaultSession:   c.defaultSession,
+	}, &request)
+	if err != nil {
+		return payload, err
+	}
+
+	if errorResponseEntity != nil {
+		return payload, errorResponseEntity
+	}
+	payload, err = confluent_util.DeserializeResponse[GetTopicResponseEntity](r.Body)
+	if err != nil {
+		return payload, err
+	}
+
+	return payload, nil
+}
+
+type ListTopicRequest struct {
+	Endpoint  string
+	ClusterID string
+}
+
+type ListTopicResponseEntity struct {
+	Kind     string `json:"kind"`
+	Metadata struct {
+		Self string      `json:"self"`
+		Next interface{} `json:"next"`
+	} `json:"metadata"`
+	Data []GetTopicResponseEntity `json:"data"`
+}
+
+func (c *TopicsClient) ListTopicRequest(session confluent_util.Session, request TopicRequestEntity[any]) (ListTopicResponseEntity, error) {
+	var payload ListTopicResponseEntity
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/%s/%s/%s", request.Endpoint, "kafka/v3/clusters", request.ClusterID, "topics"), nil)
+	if err != nil {
+		return payload, err
+	}
+	r, errorResponseEntity, err := confluent_util.DoHttpRequest(confluent_util.DoHttpRequestParameters{
+		HttpClient:       c.http,
+		Req:              req,
+		ParameterSession: session,
+		DefaultSession:   c.defaultSession,
+	}, &request)
+	if err != nil {
+		return payload, err
+	}
+
+	if errorResponseEntity != nil {
+		return payload, errorResponseEntity
+	}
+
+	payload, err = confluent_util.DeserializeResponse[ListTopicResponseEntity](r.Body)
+	if err != nil {
+		return payload, err
+	}
+
+	return payload, nil
+}
+
+type UpdateTopicRequestEntity struct {
+	Name      string `json:"name"`
+	Value     string `json:"value,omitempty"`
+	Operation string `json:"operation,omitempty"`
+}
+
+type UpdateTopicRequestPayload struct {
+	Data []UpdateTopicRequestEntity `json:"data"`
+}
+
+type UpdateTopicsRequest struct {
+	Endpoint  string
+	ClusterID string
+	TopicName string
+	Payload   UpdateTopicRequestPayload
+}
+
+func (c *TopicsClient) UpdateKafkaTopic(session confluent_util.Session, request TopicRequestEntity[UpdateTopicRequestPayload]) error {
+	requestPayload, err := json.Marshal(request.Payload)
+	if err != nil {
+		return err
+	}
+	req, err := http.NewRequest("POST", fmt.Sprintf("%s/%s/%s/%s/%s/%s", request.Endpoint, "kafka/v3/clusters", request.ClusterID, "topics", *request.TopicName, "configs:alter"), bytes.NewBuffer(requestPayload))
+	if err != nil {
+		return err
+	}
+	_, errorResponseEntity, err := confluent_util.DoHttpRequest(confluent_util.DoHttpRequestParameters{
+		HttpClient:       c.http,
+		Req:              req,
+		ParameterSession: session,
+		DefaultSession:   c.defaultSession,
+	}, &request)
+	if err != nil {
+		return err
+	}
+	if errorResponseEntity != nil {
+		return errorResponseEntity
+	}
+	return nil
 }
--- client/confluent_cloud/util/http.go
+++ client/confluent_cloud/util/http.go
@@ -2,10 +2,14 @@ package util
 
 import (
 	"encoding/json"
-	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 )
+
+func CreateStringPointer(s string) *string {
+	return &s
+}
 
 type Response struct {
 	ApiVersion string           `json:"api_version,omitempty"`
@@ -29,84 +33,107 @@ type DoHttpRequestParameters struct {
 	DefaultSession   Session
 }
 
-func DoHttpRequest[T any](req DoHttpRequestParameters) (T, error) {
-	var baseResp T
+type ErrorResponseEntity struct {
+	ErrorCode int    `json:"error_code"`
+	Message   string `json:"message"`
+}
+
+func (e *ErrorResponseEntity) Error() string {
+	return e.Message
+}
+
+type ErrorHandler interface {
+	Handle(e error, response *http.Response) (*ErrorResponseEntity, error)
+}
+
+// type DoHTTPRequestResponse struct{}
+func DoHttpRequest(req DoHttpRequestParameters, errorHandler ErrorHandler) (*http.Response, *ErrorResponseEntity, error) {
 	err := SetAuthHeader(req.Req, req.ParameterSession, req.DefaultSession)
 	if err != nil {
-		return baseResp, err
+		return nil, nil, err
 	}
 
 	resp, err := req.HttpClient.Do(req.Req)
 	if err != nil {
-		return baseResp, err
+		return resp, nil, err
 	}
 
-	respBytes, err := ioutil.ReadAll(resp.Body)
+	errorResponseEntity, err := errorHandler.Handle(err, resp)
 	if err != nil {
-		return baseResp, err
+		return nil, nil, err
 	}
 
-	err = json.Unmarshal(respBytes, &baseResp)
-	if err != nil {
-		return baseResp, err
-	}
-
-	return baseResp, nil
+	return resp, errorResponseEntity, nil
 }
 
-func HandlePagedResponse[T any](r DoHttpRequestParameters, initialResp Response) ([]T, error) {
-	payload := []T{}
-	container := []interface{}{}
-	container = append(container, initialResp.Data)
-
-	nextUrl := initialResp.Metadata.Next
-
-	for {
-		if nextUrl == "" {
-			break
-		}
-		fmt.Println(nextUrl)
-		req, err := http.NewRequest("GET", nextUrl, nil)
-		if err != nil {
-			return payload, err
-		}
-
-		nextResp, err := DoHttpRequest[Response](DoHttpRequestParameters{
-			HttpClient:       r.HttpClient,
-			Req:              req,
-			ParameterSession: r.ParameterSession,
-			DefaultSession:   r.DefaultSession,
-		})
-		if err != nil {
-			return payload, err
-		}
-
-		container = append(container, nextResp.Data)
-		nextUrl = nextResp.Metadata.Next
+func DeserializeResponse[T any](body io.ReadCloser) (T, error) {
+	var payload T
+	bodyBytes, err := ioutil.ReadAll(body)
+	if err != nil {
+		return payload, err
 	}
-
-	// Deserialise
-	// TODO: Perhaps replace with https://github.com/mitchellh/mapstructure
-	denested := []map[string]interface{}{}
-	for _, c := range container {
-		objects := c.([]interface{})
-		for _, object := range objects {
-			denested = append(denested, object.(map[string]interface{}))
-		}
+	err = json.Unmarshal(bodyBytes, &payload)
+	if err != nil {
+		return payload, err
 	}
-
-	for _, d := range denested {
-		var deserialised T
-		serialised, err := json.Marshal(d)
-		if err != nil {
-			return payload, err
-		}
-		err = json.Unmarshal(serialised, &deserialised)
-		if err != nil {
-			return payload, err
-		}
-		payload = append(payload, deserialised)
-	}
-
 	return payload, nil
 }
+
+// func HandlePagedResponse[T any](r DoHttpRequestParameters, initialResp Response) ([]T, error) { // TODO: Need testing with statuscode
+// 	payload := []T{}
+// 	container := []interface{}{}
+// 	container = append(container, initialResp.Data)
+
+// 	nextUrl := initialResp.Metadata.Next
+
+// 	for {
+// 		if nextUrl == "" {
+// 			break
+// 		}
+// 		fmt.Println(nextUrl)
+// 		req, err := http.NewRequest("GET", nextUrl, nil)
+// 		if err != nil {
+// 			return payload, err
+// 		}
+
+// 		nextResp, err := DoHttpRequest(DoHttpRequestParameters{
+// 			HttpClient:       r.HttpClient,
+// 			Req:              req,
+// 			ParameterSession: r.ParameterSession,
+// 			DefaultSession:   r.DefaultSession,
+// 		})
+// 		if err != nil {
+// 			return payload, err
+// 		}
+
+// 		// TODO: Deserialize
+
+// 		container = append(container, nextResp.Data)
+// 		nextUrl = nextResp.Metadata.Next
+// 	}
+
+// 	// Deserialise
+// 	// TODO: Perhaps replace with https://github.com/mitchellh/mapstructure
+// 	denested := []map[string]interface{}{}
+// 	for _, c := range container {
+// 		objects := c.([]interface{})
+// 		for _, object := range objects {
+// 			denested = append(denested, object.(map[string]interface{}))
+// 		}
+// 	}
+
+// 	for _, d := range denested {
+// 		var deserialised T
+// 		serialised, err := json.Marshal(d)
+// 		if err != nil {
+// 			return tempStatusCode, payload, err
+// 		}
+// 		err = json.Unmarshal(serialised, &deserialised)
+// 		if err != nil {
+// 			return tempStatusCode, payload, err
+// 		}
+// 		payload = append(payload, deserialised)
+// 	}
+
+// 	return payload, nil
+// }
